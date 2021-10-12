1c1ec12fc8fda0d6510693b61fb9aa3a40f93baa
IKIK20 <67692637+IKIK20@users.noreply.github.com>
2021-10-12 09:25:25 +0530

    Add files via upload

---
+++ swappingFile.py
@@ -0,0 +1,17 @@
+def swapFileData():
+    file1=input("enter the name of file1:- ")
+    file2=input("enter the name of file2:- ")
+
+    with open( file1, 'r') as a:
+        data_a=a.read()
+
+    with open( file2, 'r') as a:
+        data_b=a.read()
+
+    with open( file1, 'w') as a:
+        a.write(data_b)
+    
+    with open( file1, 'w') as a:
+        a.write(data_a)
+
+swapFileData()
